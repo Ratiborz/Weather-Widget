a736df63c61f7f6bfe8b7c74e1ce0d86a0d74f9c
Heorhi Zaturkevich <konisgung@gmail.com>
2023-12-11 23:51:44 +0300

    Configuring react

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
-import React from "react";
-    import User from "./Components/User";
 
-    function App() {
-     const user = {
-       name: 'Ivan',
-       secondName: 'Petrov',
-       age: '25',
-       gender: 'М',
-       rank: 3
-     }
-     return (
-       <div>
-         <User {...user}/>
-       </div>
-     );
-    }
-    export default App;
-
-function ImageContainer() {
+function App() {
   return (
-    <div>
-      <img src="https://www.vkruzhevah.ru/upload/resize_cache/webp/upload/iblock/0d9/mari.webp" alt='Изображение для статьи'/>
-      <span>Крутой подзаголовок для нашей статьи</span>
-    </div>
-  )
-}
 
-/*function App() {
-  return (
-    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div> 
-
-    <section>
-      <h1>React ebash</h1>
-      <ImageContainer/>
-      <ImageContainer/>
-      <ImageContainer/>
-    </section>
-    </>
   ); 
 }
 
-export default App; */
+export default App; 
--- src/Components/User/index.jsx
+++ src/Components/User/index.jsx
@@ -1,39 +1,56 @@
 import React, { useState } from "react";
 import style from "./style.module.css";
 
-const [rankState, setRank] = useState(rank);
+const User = ({name, secondName, age, gender, rank}) => {
 
-const handlerIncrease = () => setRank(rankState + 1);
+  const [rankState, setRank] = useState(rank);
+  const handlerIncrease = (e) => {
+    console.log(e)
+    setRank(rankState + 1)
+  }
 
-const handlerDecrease = () => setRank(rankState - 1);
+async function apiRequests() {
+  try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=aa302d9baf187e648240b52a933d9fdc`;
+      const response = await fetch(url);
 
-    const User = ({name, secondName, age, gender, rank}) => {
-        return (
-          <div className={style.UserCard}>
-            <div className={style.Title}>
-              {`Пользователь: ${name} ${secondName}`}
-            </div>
-            <div>
-              <div className={style.Title}>
-                Информация:
-              </div>
-              <div>
-                {`Возраст: ${age}`}
-              </div>
-              <div>
-                {`Пол: ${gender}`}
-              </div>
-            </div>
-            <div /*className={style.Title}*/ style={".title"}>
-              Рейтинг {rank}
-            </div>
-            <span>{rank}</span>
-            <div>
-              <button onClick={handlerIncrease}>+</button>
-              <button onClick={handlerDecrease}>-</button>
-            </div>
+      /*if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }*/
+
+      const data = await response.json();
+      return console.log(data);
+  } catch (error) {
+      console.error('Error fetching data: ', error);
+  }
+} 
+
+    return (
+      <div className={style.UserCard}>
+        <div className={style.Title}>
+          {`Пользователь: ${name} ${secondName}`}
+        </div>
+        <div>
+          <div className={style.Title}>
+            Информация:
           </div>
-        )
-    }
+          <div>
+            {`Возраст: ${age}`}
+          </div>
+          <div>
+            {`Пол: ${gender}`}
+          </div>
+        </div>
+        <div className={style.Title}>
+          Рейтинг {rank}
+        </div>
+        <span>{rank}</span>
+        <div>
+          <button onClick={(e)  => handlerIncrease(e)}>+</button>
+          <button onClick={apiRequests}>-</button>
+        </div>
+      </div>
+    )
+}
        
-    export default User
+export default User
